37453d6b44832bb25165944e9deda3a2f3c5cb39
ak1421 <ak1421@uni-rostock.de>
2022-08-16 13:25:26 +0000

    Add new file

---
+++ J2XML/SimpleErrorHandler.java
@@ -0,0 +1,19 @@
+import org.xml.sax.ErrorHandler;
+import org.xml.sax.SAXException;
+import org.xml.sax.SAXParseException;
+
+public class SimpleErrorHandler implements ErrorHandler {
+
+	public void warning(SAXParseException exception) throws SAXException {
+
+	}
+
+	public void error(SAXParseException exception) throws SAXException {
+
+	}
+
+	public void fatalError(SAXParseException exception) throws SAXException {
+
+	}
+
+}
