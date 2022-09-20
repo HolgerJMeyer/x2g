35c2b2605583069a920565859f437b1596014e17
ak1421 <ak1421@uni-rostock.de>
2022-09-20 17:44:27 +0000

    Update J2XML.java

--- J2XML/J2XML.java
+++ J2XML/J2XML.java
@@ -10,6 +10,7 @@ import javax.xml.parsers.DocumentBuilderFactory;
 import javax.xml.parsers.ParserConfigurationException;
 
 import org.apache.maven.model.InputSource;
+import org.json.simple.JSONArray;
 import org.json.simple.JSONObject;
 import org.json.simple.parser.JSONParser;
 import org.json.simple.parser.ParseException;
@@ -19,44 +20,75 @@ public class J2XML {
 	public org.w3c.dom.Document document;
 	public J2XML(File JSON_File) throws IOException, ParseException, ParserConfigurationException, SAXException {
 		if(JSON_File.isFile() &&  JSON_File.getName().endsWith(".json")&& JSON_File.length() > 0) {
-			JSONParser jp=new JSONParser();
-		    JSONObject jo= (JSONObject) jp.parse(new FileReader(JSON_File));
-		    //jo=(JSONObject) jp.parse(new FileReader("C:\\Users\\Arthur\\Downloads\\sample2.json"));
-		    String json_value=jo.toString();
-		   // System.out.println(json_value);
-		    Convert_JSON_TO_XML c= new Convert_JSON_TO_XML();
-		   
-		    if( c.isJSONWellFormed(json_value)) {
-		    	//System.out.print(c.isJSONWellFormed(json_value));
-		    	 String res=c.convert_json(json_value);
-				    System.out.print(res);
-			       this.XML_File=new File( fileName(JSON_File.getName())+".xml");
-				    if((!this.XML_File.exists())) {
-				    	this.XML_File.createNewFile();
-				    }
-				    PrintWriter pw=new PrintWriter(this.XML_File);
-				    pw.println(res);
-				    pw.close();
-				    DocumentBuilderFactory factory = DocumentBuilderFactory.newInstance();
-				    factory.setValidating(false);
-				    factory.setNamespaceAware(true);
-
-				    DocumentBuilder builder = factory.newDocumentBuilder();
-
-				    builder.setErrorHandler(new SimpleErrorHandler());    
-				     //the "parse" method also validates XML, will throw an exception if misformatted
-				    this.document =  builder.parse(this.XML_File);
-				    Scanner sc = new Scanner(this.XML_File);
-				      String input;
-				      StringBuffer sb = new StringBuffer();
-				      while (sc.hasNextLine()) {
-				         input = sc.nextLine();
-				         sb.append(" "+input);
-				      }
-				     // System.out.println(sb.toString());
-				   
-				
-		    }
+			Convert_JSON_TO_XML c= new Convert_JSON_TO_XML();
+			Scanner sc = new Scanner(JSON_File);
+		      String input;
+		      StringBuffer sb = new StringBuffer();
+		      while (sc.hasNextLine()) {
+		         input = sc.nextLine();
+		         sb.append(" "+input);
+		      }   
+			if(c.isJSONWellFormed_array(sb.toString())) {
+		    	  JSONParser jp=new JSONParser();
+					JSONArray jo=  (JSONArray) jp.parse(new FileReader(JSON_File));
+				    //jo=(JSONObject) jp.parse(new FileReader("C:\\Users\\Arthur\\Downloads\\sample2.json"));
+				    String json_value=jo.toString();
+				    	 String res= c.convert_jsonArray(json_value);
+						  // System.out.print(res);
+					       this.XML_File=new File( fileName(JSON_File.getName())+".xml");
+						    if((!this.XML_File.exists())) {
+						    	this.XML_File.createNewFile();
+						    }
+						    PrintWriter pw=new PrintWriter(this.XML_File);
+						    pw.println(res);
+						    pw.close();
+						    DocumentBuilderFactory factory = DocumentBuilderFactory.newInstance();
+						    factory.setValidating(false);
+						    factory.setNamespaceAware(false);
+						    DocumentBuilder builder = factory.newDocumentBuilder();
+						    builder.setErrorHandler(new SimpleErrorHandler());    
+						     //the "parse" method also validates XML, will throw an exception if misformatted
+						    this.document =  builder.parse(this.XML_File);
+						    Scanner scc = new Scanner(this.XML_File);
+						      String inputt;
+						      StringBuffer sbb = new StringBuffer();
+						      while (scc.hasNextLine()) {
+						         inputt = scc.nextLine();
+						         sbb.append(" "+inputt);
+						      }
+						      System.out.println(sbb.toString());
+				    }  
+		      if(c.isJSONWellFormed_object(sb.toString())) {
+		    	  
+		    	  JSONParser jp=new JSONParser();
+					JSONObject jo=  (JSONObject) jp.parse(new FileReader(JSON_File));
+				    //jo=(JSONObject) jp.parse(new FileReader("C:\\Users\\Arthur\\Downloads\\sample2.json"));
+				    String json_value=jo.toString();
+				    	 String res=c.convert_jsonObject(json_value);
+				    	//String ns= c.add_NameSpace(res, "cd", "b", "https://www.w3schools.com/XML/xml_namespaces.asp");
+						// System.out.print(res);
+					       this.XML_File=new File( fileName(JSON_File.getName())+".xml");
+						    if((!this.XML_File.exists())) {
+						    	this.XML_File.createNewFile();
+						    }
+						    PrintWriter pw=new PrintWriter(this.XML_File);
+						    pw.println(res);
+						    pw.close();
+						    DocumentBuilderFactory factory = DocumentBuilderFactory.newInstance();
+						    factory.setValidating(false);
+						    factory.setNamespaceAware(false);
+						    DocumentBuilder builder = factory.newDocumentBuilder();
+						    builder.setErrorHandler(new SimpleErrorHandler());    
+						    this.document =  builder.parse(this.XML_File);
+						    Scanner scc = new Scanner(this.XML_File);
+						      String inputt;
+						      StringBuffer sbb = new StringBuffer();
+						      while (scc.hasNextLine()) {
+						         inputt = scc.nextLine();
+						         sbb.append(" "+inputt);
+						      }
+						      System.out.println(sbb.toString());
+						      }
 		} else {
 			System.out.println("your File is not a JSON_File");
 		}
@@ -74,13 +106,14 @@ public class J2XML {
 	}
 	
 	public static void main(String[] args) throws FileNotFoundException, IOException, ParseException, ParserConfigurationException, SAXException {
-   //J2XML test= new J2XML(new File("C:\\Users\\Arthur\\Downloads\\sample1.json"));
+   J2XML test= new J2XML(new File("C:\\Users\\Arthur\\Downloads\\xmltojson.json"));
 		 Convert_JSON_TO_XML test1= new Convert_JSON_TO_XML();
-		 
-   J2XML test3= new J2XML(test1.Get_JSON_Files("http://api.wossidia.de/catalog/edgetypes"));
-	 //  File test1 = test3.XML_File;
-	    //J2XML test2= new J2XML(test1);
+		 test1.JPTOXP("[?(@.color == \"blue\")]");
+  //J2XML test3= new J2XML(test1.Get_JSON_Files("http://api.wossidia.de/graph/180011349/%3C1019%3E101%3C101%3E102%3C102%3E103%3C342%3E17%3C1028%3E29"));
+	   //File test5 = test.XML_File;
+	    //J2XML test2= new J2XML(test5);
 	//    System.out.println(test1.getName());
 	   // String z = Get_JSON_Files();
 		//System.out.print(z);
+		 
 }}
