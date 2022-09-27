81bc8260b73cd3eb9ad162a505b21c047ed56706
Holger J Meyer <hm@IEEE.org>
2022-09-27 15:44:18 +0200

    Differnt warning types introduced

---
+++ src/x2gParser/WarnType.java
@@ -0,0 +1,11 @@
+public class WarnType {
+	public static final int
+		NONE = 0,
+		GENERAL = 1,
+		EMPTYSET = 2,
+		MISSINGNODES = 4,
+		MISSINGKEYVALUE = 8,
+		ALL = 256-1;
+}
+
+// vim: ff=unix ts=3 sw=3 sts=3 noet
